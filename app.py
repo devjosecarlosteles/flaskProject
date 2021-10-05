543377dea56edb7c3407f837ddc037d5ef5f035b
devjosecarlosteles <devjosecarlosteles@gmail.com>
2021-10-04 23:55:09 -0300

    query params

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 
 app = Flask("flaskProject")
 
+
 @app.route("/")
 def hello_world():
     return render_template("index.html"), 200
@@ -12,12 +13,11 @@ def about():
     return render_template("about/index.html"), 200
 
 
-@app.route("/profile")
-def profile():
+@app.route("/profile/<name>/<email>")
+def profile(name, email):
     return render_template("profile/index.html",
-        name="José Carlos Teles",
-        email="devjosecarlosteles@gmail.com"
-    ), 200
+        name=name,
+        email=email), 200
 
 
 app.run()
